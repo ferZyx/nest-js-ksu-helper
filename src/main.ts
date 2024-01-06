878d0f325abebf858cf2cbe2e247e3209ffc4378
ferZyx <sorokin.wlad@yandex.ru>
2024-01-06 18:28:37 +0600

    added api global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,25 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from './app.module';
-import * as process from "process";
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as process from 'process'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-    const PORT = process.env.PORT || 5000
-    const app = await NestFactory.create(AppModule);
+	const PORT = process.env.PORT || 5000
+	const app = await NestFactory.create(AppModule)
 
-    const config =  new DocumentBuilder()
-        .setTitle("Ето название")
-        .setDescription("Владик смог запусить сваггер")
-        .setVersion("1.0.0")
-        .build()
-    const document = SwaggerModule.createDocument(app, config)
-    SwaggerModule.setup('/api/docs', app, document)
+	app.setGlobalPrefix('api')
 
-    await app.listen(PORT).then(() => console.log(`Server started on ${PORT} port`));
+	const config = new DocumentBuilder()
+		.setTitle('Ето название')
+		.setDescription('Владик смог запусить сваггер')
+		.setVersion('1.0.0')
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('/api/docs', app, document)
+
+	await app
+		.listen(PORT)
+		.then(() => console.log(`Server started on ${PORT} port`))
 }
 
-bootstrap();
+bootstrap()
