e4a6f668011e037b77e765310a70f0c54669b84d
ferZyx <sorokin.wlad@yandex.ru>
2024-01-08 12:12:42 +0600

    bux fixing

--- src/users/dtos/login-user.dto.ts
+++ src/users/dtos/login-user.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty } from 'class-validator'
 
 export class LoginUserDto {
 	@ApiProperty({ example: 'user@gmail.com', description: 'Почтовый адрес' })
+	@IsNotEmpty({ message: 'Не указана почта' })
 	readonly email: string
 
 	@ApiProperty({ example: '123123', description: 'Пароль' })
+	@IsNotEmpty({ message: 'Не указан пароль' })
 	readonly password: string
 }
