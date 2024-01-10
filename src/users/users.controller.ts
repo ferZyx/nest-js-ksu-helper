3c916e665451726619ce5cc3f1399c1bec3c7455
ferZyx <sorokin.wlad@yandex.ru>
2024-01-10 19:42:15 +0600

    deleted create user route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,13 @@
 import {
-	Body,
 	ClassSerializerInterceptor,
 	Controller,
 	Get,
 	HttpStatus,
-	Post,
 	Req,
 	UseGuards,
 	UseInterceptors
 } from '@nestjs/common'
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
-import { CreateUserDto } from './dtos/create-user.dto'
 import { User } from '../schemas/user.schema'
 import { UsersService } from './users.service'
 import { RolesAuthGuard } from '../auth/roles-auth.guard'
@@ -22,23 +19,6 @@ import { UserDto } from './dtos/user.dto'
 @Controller('users')
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
-
-	@ApiOperation({ summary: 'Создать пользователя. Доступно админам' })
-	@ApiResponse({
-		type: User,
-		status: HttpStatus.CREATED,
-		description: 'Пользователь успешно создан'
-	})
-	@ApiResponse({
-		status: HttpStatus.CONFLICT,
-		description: 'Пользователь с таким email-ом уже создан'
-	})
-	@Roles('Admin')
-	@UseGuards(RolesAuthGuard)
-	@Post()
-	create(@Body() userDto: CreateUserDto) {
-		return this.usersService.createUser(userDto)
-	}
 
 	@ApiOperation({ summary: 'Получить всех пользователей. Доступно админам' })
 	@ApiResponse({
