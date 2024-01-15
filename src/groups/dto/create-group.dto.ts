694f4c1491014103a16f9909d004058de7094673
ferZyx <sorokin.wlad@yandex.ru>
2024-01-15 15:59:29 +0600

    Formatted dto

--- src/groups/dto/create-group.dto.ts
+++ src/groups/dto/create-group.dto.ts
@@ -4,12 +4,20 @@ export class CreateGroupDto {
 	@IsString({ message: 'Должно быть строкой' })
 	@IsNotEmpty({ message: 'Не должно быть пустым' })
 	readonly name: string
+
 	@IsString({ message: 'Должно быть строкой' })
 	@IsNotEmpty({ message: 'Не должно быть пустым' })
 	readonly type: string
+
 	@IsOptional()
 	owner?: string
+
+	@IsOptional()
 	members?: string[]
+
+	@IsOptional()
 	admins?: string[]
+
+	@IsOptional()
 	joinRequests?: string[]
 }
