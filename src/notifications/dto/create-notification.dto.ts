2be79e6327a6a917c27a222e013b42fe4b4c29cf
ferZyx <sorokin.wlad@yandex.ru>
2024-03-09 23:00:13 +0500

    Changed error messages

--- src/notifications/dto/create-notification.dto.ts
+++ src/notifications/dto/create-notification.dto.ts
@@ -1,15 +1,14 @@
 import { IsNotEmpty, IsString } from 'class-validator'
 import { ApiProperty } from '@nestjs/swagger'
-import { Expose } from 'class-transformer'
 
 export class CreateNotificationDto {
 	@ApiProperty({ example: 'Новое уведомление', description: 'Заголовок' })
-	@IsNotEmpty({ message: 'Не должно быть пустым' })
+	@IsNotEmpty({ message: 'Заголовок не может быть пустым' })
 	@IsString({ message: 'Должно быть строкой' })
 	readonly title: string
 
 	@ApiProperty({ example: 'Текст уведомления', description: 'Сообщение' })
-	@IsNotEmpty({ message: 'Не должно быть пустым' })
+	@IsNotEmpty({ message: 'Сообщение не может быть пустым' })
 	@IsString({ message: 'Должно быть строкой' })
 	readonly message: string
 
@@ -17,10 +16,7 @@ export class CreateNotificationDto {
 		example: '6597d87f09da1c01b87fe7d7',
 		description: 'Уникальный идентификатор пользователя'
 	})
-	@IsNotEmpty({ message: 'Не должно быть пустым' })
+	@IsNotEmpty({ message: 'Поле user не может быть пустым' })
 	@IsString({ message: 'Должно быть строкой' })
 	readonly user: string
-
-	@Expose()
-	readonly hasRead: boolean
 }
