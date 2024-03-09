eb9ffa84740c1cc7de592fbefd993cb1ca7b550a
ferZyx <sorokin.wlad@yandex.ru>
2024-03-09 23:01:40 +0500

    Documented with swagger

--- src/notifications/entities/notification.entity.ts
+++ src/notifications/entities/notification.entity.ts
@@ -5,16 +5,18 @@ import { ApiProperty } from '@nestjs/swagger'
 export class NotificationEntity {
 	@ApiProperty({
 		example: '6597d87f09da1c01b87fe7d7',
-		description: 'Уникальный идентификатор'
+		description: 'Уникальный идентификатор уведомления'
 	})
 	@Transform(({ value }) => value.toString())
 	_id: Types.ObjectId
 
+	@ApiProperty({ example: 'Новое уведомление', description: 'Заголовок' })
 	readonly title: string
 
+	@ApiProperty({ example: 'Текст уведомления', description: 'Сообщение' })
 	readonly message: string
 
-	@Exclude()
+	@Transform(({ value }) => value.toString())
 	readonly user: Types.ObjectId[]
 
 	readonly hasRead: boolean
