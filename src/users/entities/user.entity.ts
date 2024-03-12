35f1e6d1940507716ea12f9e868778ebe5aaf008
ferZyx <sorokin.wlad@yandex.ru>
2024-03-12 16:55:45 +0500

    fix

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,37 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { Exclude, Transform, Type } from 'class-transformer'
+import { Types } from 'mongoose'
+import { UserNotificationEntity } from '../../notifications/entities/user-notification.entity'
+import { RoleEntity } from '../../roles/entities/role.entity'
+
 export class UserEntity {
-	// @ApiProperty({ example: 'user@gmail.com', description: 'Почтовый адрес' })
-	// readonly email: string
-	//
-	// @Transform(({ value }) => value?.toString() || null)
-	// readonly group: Types.ObjectId
-	//
+	@ApiProperty({ example: 'user@gmail.com', description: 'Почтовый адрес' })
+	readonly email: string
+
+	@Transform(({ value }) => value?.toString() || null)
+	readonly group: Types.ObjectId
+
 	readonly groupRole: string
 
-	// @Type(() => UserNotificationEntity)
-	// readonly notifications: Notification[]
+	@Type(() => UserNotificationEntity)
+	readonly notifications: Notification[]
 
-	// @ApiProperty({ example: '123123', description: 'Пароль' })
-	// @Exclude()
-	// readonly password: string
-	//
-	// @ApiProperty({
-	// 	example: '6597d87f09da1c01b87fe7d7',
-	// 	description: 'Уникальный идентификатор'
-	// })
-	// @Transform(({ value }) => value?.toString())
-	// readonly _id: Types.ObjectId
+	@ApiProperty({ example: '123123', description: 'Пароль' })
+	@Exclude()
+	readonly password: string
 
-	// @ApiProperty({
-	// 	example: 'User',
-	// 	description: 'Роли пользователя',
-	// 	type: [RoleEntity]
-	// })
-	// @Transform(({ value }) => value.map((role: { name: string }) => role.name))
-	// @Type(() => RoleEntity)
-	// readonly roles: RoleEntity[]
+	@ApiProperty({
+		example: '6597d87f09da1c01b87fe7d7',
+		description: 'Уникальный идентификатор'
+	})
+	@Transform(({ value }) => value?.toString())
+	readonly _id: Types.ObjectId
 
-	// @Exclude()
-	// readonly __v: number
+	@ApiProperty({
+		example: 'User',
+		description: 'Роли пользователя',
+		type: [RoleEntity]
+	})
+	@Transform(({ value }) => value.map((role: { name: string }) => role.name))
+	@Type(() => RoleEntity)
+	readonly roles: RoleEntity[]
+
+	@Exclude()
+	readonly __v: number
 
 	constructor(partial: Partial<UserEntity>) {
 		Object.assign(this, partial)
